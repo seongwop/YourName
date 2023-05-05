c9f1d209d01859e2cd347319e54192a632bb6132
bheejun <bhjun95@gmail.com>
2023-05-05 17:13:54 +0900

    프로젝트생성
    프로젝트생성

---
+++ src/main/java/com/sparta/yourname/YourNameApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.yourname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourNameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourNameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/yourname/YourNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.yourname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourNameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
