a4ed42b2e8185f40ca0c6537f05e6a40bb8e2e97
PYJ <june9152@gmail.com>
2023-05-07 18:31:19 +0900

    user Class 변경 적용 / Build Gradle implement 추가

--- src/main/java/com/sparta/yourname/controller/MyPageController.java
+++ src/main/java/com/sparta/yourname/controller/MyPageController.java
@@ -24,7 +24,7 @@ public class MyPageController {
 
     //개인 정보 수정
     @PutMapping
-    public ResponseEntity<UserResponseDto> updatePersonalInformation(@RequestParam String userId, @RequestBody UserRequestDto updatedInfo) {
+    public ResponseEntity<UserResponseDto> updatePersonalInformation(@RequestParam String userId, @RequestBody UserRequestDto.info updatedInfo) {
         UserResponseDto updatedUser = myPageService.updateInformation(userId, updatedInfo);
         return ResponseEntity.ok(updatedUser);
     }
