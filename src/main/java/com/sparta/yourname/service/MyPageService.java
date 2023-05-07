12c42bdf4724363606218fe59324a15b1bdb003b
PYJ <june9152@gmail.com>
2023-05-07 17:58:02 +0900

    Error 처리 수정을 위한 주석 해제

--- src/main/java/com/sparta/yourname/service/MyPageService.java
+++ src/main/java/com/sparta/yourname/service/MyPageService.java
@@ -21,11 +21,11 @@ public class MyPageService {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
         // 업데이트
-//        user.setEmail(updatedInfo.getEmail());
-//        user.setSpecialty(updatedInfo.getSpecialty());
-//        user.setMbti(updatedInfo.getMbti());
-//        user.setGithuburl(updatedInfo.getGithubUrl());
-//        user.setBlogurl(updatedInfo.getBlogUrl());
+        user.setEmail(updatedInfo.getEmail());
+        user.setSpecialty(updatedInfo.getSpecialty());
+        user.setMbti(updatedInfo.getMbti());
+        user.setGithuburl(updatedInfo.getGithubUrl());
+        user.setBlogurl(updatedInfo.getBlogUrl());
 
         // 업데이트한 정보를 저장
         User updatedUser = userRepository.save(user);
