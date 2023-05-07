98a2398a012ae3f2cc4c617eedf03f70a0263788
PYJ <june9152@gmail.com>
2023-05-07 14:29:11 +0900

    최신 파일과 Merge 및 Error 표시 BuildGradle 추가

--- src/main/java/com/sparta/yourname/service/MyPageService.java
+++ src/main/java/com/sparta/yourname/service/MyPageService.java
@@ -20,13 +20,12 @@ public class MyPageService {
     public UserResponseDto updateInformation(String userId, UserRequestDto updatedInfo) {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
-
         // 업데이트
-        user.setEmail(updatedInfo.getEmail());
-        user.setSpecialty(updatedInfo.getSpecialty());
-        user.setMbti(updatedInfo.getMbti());
-        user.setGithuburl(updatedInfo.getGithubUrl());
-        user.setBlogurl(updatedInfo.getBlogUrl());
+//        user.setEmail(updatedInfo.getEmail());
+//        user.setSpecialty(updatedInfo.getSpecialty());
+//        user.setMbti(updatedInfo.getMbti());
+//        user.setGithuburl(updatedInfo.getGithubUrl());
+//        user.setBlogurl(updatedInfo.getBlogUrl());
 
         // 업데이트한 정보를 저장
         User updatedUser = userRepository.save(user);
