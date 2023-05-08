24220966a08af001bfdbc43e66fdee9efb305fca
bheejun <128471362+bheejun@users.noreply.github.com>
2023-05-08 23:05:08 +0900

    [FIX}: UserRepository 수정
    필요없는 부분 삭제

--- src/main/java/com/sparta/yourname/repository/UserRepository.java
+++ src/main/java/com/sparta/yourname/repository/UserRepository.java
@@ -2,13 +2,12 @@ package com.sparta.yourname.repository;
 
 
 import com.sparta.yourname.entity.User;
-<<<<<<< HEAD
-=======
+
 
 import jakarta.persistence.Table;
->>>>>>> 5485b4a9536aec3fe9f6487f8f4520649c62d6ba
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.Optional;
 
