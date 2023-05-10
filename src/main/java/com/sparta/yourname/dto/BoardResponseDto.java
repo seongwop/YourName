188f5cf22fd28e246067b3b8f3e8684e5fc2ba3b
joshWPark <parkjoshua98@gmail.com>
2023-05-10 13:04:14 +0900

    BoardResponseDto에 ID 추가

--- src/main/java/com/sparta/yourname/dto/BoardResponseDto.java
+++ src/main/java/com/sparta/yourname/dto/BoardResponseDto.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class BoardResponseDto {
+    private Long id;
     private String title;
     private String content;
     private String author;
 
 
     public BoardResponseDto(Board board) {
+        this.id = board.getId();
         this.title = board.getTitle();
         this.content = board.getContent();
         this.author = board.getUser().getUsername();
